306bd5989d6cf0fa5d85ff3b56a1b77d16366653
Henrique <henrijv@hotmail.com>
2025-03-20 20:14:12 -0300

    Alteração bombastica

--- aula06/src/Conta.java
+++ aula06/src/Conta.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
+import java.util.Objects;
 
-public class Conta {
+public class Conta{
+
+    public String name;
+    public Integer numero;
+    public Integer agencia;
+    public double saldo;
+    public LocalDate dataCriacao;
+
+    public void depositar(double valor){
+        this.saldo += valor;
+    }
+
+    public boolean sacar(double valor){
+        if(this.saldo > valor){
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public void calcularRendimento(){
+        this.saldo = this.saldo * 1.10;
+    }
+
+    public String recuperarDadosParaImpressao(){
+        return String.format("[agencia: %d, número: %d, Nome: %s, Data Criação: %s, Saldo: %f]",
+                agencia, numero, name, dataCriacao.toString(), saldo);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return Objects.equals(numero, conta.numero) && Objects.equals(agencia, conta.agencia);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numero, agencia);
+    }
 }
--- aula06/src/TesteConta.java
+++ aula06/src/TesteConta.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
 public class TesteConta {
+    public static void main(String[]args){
+
+        Conta contaAluno = new Conta();
+        contaAluno.name = "João";
+        contaAluno.numero = 33658;
+        contaAluno.agencia = 9999;
+        contaAluno.saldo = 0.0;
+        contaAluno.dataCriacao = LocalDate.now();
+
+        Conta contaProfessor = new Conta();
+        contaProfessor.name = "João";
+        contaProfessor.numero = 33658;
+        contaProfessor.agencia = 9999;
+        contaProfessor.saldo = 0.0;
+        contaProfessor.dataCriacao = LocalDate.now();
+
+        //contaAluno.depositar(100.00);
+        //contaAluno.sacar(1000.00);
+        //contaAluno.calcularRendimento();
+        System.out.println(contaAluno.recuperarDadosParaImpressao());
+        System.out.println(contaProfessor.recuperarDadosParaImpressao());
+
+        // comparando usando ==
+        if(contaAluno == contaProfessor){
+            System.out.println("SIM são iguais");
+        }else{
+            System.out.println("NÃO são iguais");
+        }
+
+        if(contaAluno.equals(contaProfessor)){
+            System.out.println("SIM são iguais");
+        }else{
+            System.out.println("NÃO são iguais");
+        }
+    }
 }
